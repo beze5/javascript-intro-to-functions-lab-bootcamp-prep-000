e3d61fbb344346065ea3b5cbe79ba4e33c950713
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-21 12:27:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ uppercase.toUpperCase() === uppercase
 function sayHiToGrandma(string){
     if (string === "I love you, Grandma.")
       return "I love you, too."
-    else if (string === string.toUpperCase)
+    else if (string === uppercase)
       return ("YES INDEED!")
     else (string === lowercase)
       return ("I can't hear you!")
