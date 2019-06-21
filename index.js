210b63928b4b81be076689af68235ab41325bfb5
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-21 12:40:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
       return "I love you, too."}
     else if (string.toUpperCase()){
       return 'YES INDEED!'}
-    else (string.toLowerCase())
+    else (string === string.toLowerCase())
       return ("I can't hear you!")
 }
 
